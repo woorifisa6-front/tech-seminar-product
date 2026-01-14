499bbf1bfa41eb2dce3a2ae810c3c5cc4b8206af
Jay Park <pjh6183@naver.com>
2026-01-14 12:28:52 +0900

    상단 nav bar 공통

--- WonBank/src/main.jsx
+++ WonBank/src/main.jsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import React from 'react';
+import './styles/tokens.css';
+import './styles/globals.css';
+import './styles/ui.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
